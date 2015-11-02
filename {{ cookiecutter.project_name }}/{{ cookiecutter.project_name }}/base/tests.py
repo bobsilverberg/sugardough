7cb9536274514e58486f6b2d9dc014a278530d64
Bob Silverberg <bob.silverberg@gmail.com>
2015-11-02 10:32:25 -0500

    Add test for fe filter

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/base/tests.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/base/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
@@ -7,6 +8,10 @@ class HomeTests(TestCase):
     def test_base(self):
         response = self.client.get(reverse('home'))
         assert b'csrfmiddlewaretoken' in response.content
+
+    def test_fe_filter(self):
+        response = self.client.get(reverse('home'))
+        assert 'DEBUG setting: {0}'.format(settings.DEBUG) in response.content
 
 
 class TestContribute(TestCase):
